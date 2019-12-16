65ad9d296db5631402df84f00b79d6fd96932105
Fidel Lim <lim.fidel@gmail.com>
2019-12-16 22:19:40 +0400

    add custom cell

---
+++ Whatsapp-IOS/ChatsViewControllerTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ChatsViewControllerTableViewCell.swift
+//  Whatsapp-IOS
+//
+//  Created by Fidel Lim on 12/16/19.
+//  Copyright © 2019 cybermonkeys. All rights reserved.
+//
+
+import UIKit
+
+class ChatsViewControllerTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var myMessage: UILabel!
+    @IBOutlet weak var myDate: UILabel!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        myImage.layer.cornerRadius = myImage.frame.size.width/2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
